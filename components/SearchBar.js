f75595a328aa74f1c5a3df233637e7fac74c7221
CyraMoss <cyramoss@icloud.com>
2023-03-11 20:16:19 +1300

    changed header to seperate component

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -10,19 +10,42 @@ const SearchBar = ({
 }) => {
   return (
     <View style={styles.searchBarContainer}>
-      <Input
-        placeholder="Search for your next drink spot!"
-        value={searchQuery}
-        onChangeText={setSearchQuery}
-        onSubmitEditing={handleSearch}
-        autoFocus={true}
-        containerStyle={styles.searchBarInputContainer}
-        inputContainerStyle={styles.searchBarInput}
-        leftIcon={<Icon name="search" color="#86939e" />}
-        rightIcon={
-          <Icon name="close" color="#86939e" onPress={handleSearchBarCancel} />
-        }
-      />
+            <Autocomplete
+              placeholder="Search for your next drink spot!"
+              value={searchQuery}
+              onChangeText={(text) => {
+                setSearchQuery(text);
+                setSearchTerm(text);
+              }}
+              onSubmitEditing={handleSearch}
+              autoFocus={true}
+              containerStyle={styles.searchBarInputContainer}
+              inputContainerStyle={styles.searchBarInput}
+              data={suggestions}
+              flatListProps={{
+                keyExtractor: (item, index) => index.toString(),
+                keyboardShouldPersistTaps: 'always',
+                keyExtractor: (_, idx) => idx,
+                renderItem: ({ item }) => (
+                  <TouchableOpacity
+                    onPress={() => {
+                      setSearchQuery(item.companyname);
+                      setSelectedBarCoordinate({
+                        latitude: item.location.coordinates[0],
+                        longitude: item.location.coordinates[1],
+                        latitudeDelta: 0.0222,
+                        longitudeDelta: 0.0221,
+                      });
+                      setShowSearchBar(false);
+                      3;
+                    }}
+                  >
+                    <Text>{item.companyname}</Text>
+                  </TouchableOpacity>
+                ),
+              }}
+            />
+          </View>
     </View>
   );
 };
@@ -30,10 +53,6 @@ const SearchBar = ({
 const styles = {
   searchBarContainer: {
     backgroundColor: '#fff',
-  },
-  searchBarInputContainer: {
-    backgroundColor: 'green',
-    borderRadius: 5,
   },
   searchBarInput: {
     borderBottomWidth: 0,
